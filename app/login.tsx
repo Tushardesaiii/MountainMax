076e12dbed531c2cef0e6e3507ec8a0f9163d141
Tushar Desai <imtushardesai@gmail.com>
2026-04-20 18:16:37 +0530

    login page

--- app/login.tsx
+++ app/login.tsx
@@ -22,19 +22,22 @@ export default function LoginScreen() {
       colors={["#d7f1ff", "#edf8ff", "#f9fcff"]}
       className="flex-1 px-6 pt-16"
     >
+
+      
       <View className="flex-1 justify-between pb-10">
         <View>
-          <View className="h-20 w-20 items-center justify-center rounded-3xl bg-white/85">
-            <Ionicons name="trail-sign-outline" size={40} color="#0f766e" />
-          </View>
+         
 
-          <Text className="mt-8 text-4xl font-semibold leading-tight text-slate-900">
-            Welcome to MountainMax
+          <Text className="mt-0  text-4xl font-light leading-tight text-slate-900">
+            Welcome to <span className="font-bold text-teal-700 ">Mountain<span className="font-bold text-black ">Max</span></span>
           </Text>
-          <Text className="mt-4 text-base leading-7 text-slate-700">
+          <Text className="mt-4 mb-4 text-base  leading-7 text-slate-700">
             Sign in to start scanning mountains, saving discoveries, and getting
             smarter adventure insights.
           </Text>
+          {/* image embadding  */}
+          <img src="https://imgs.search.brave.com/q5Xnkgsl03TdDu-Wl6psAmNEMs_QNF3SR0rtCYDvAkc/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE2/MDQ0Mjg4MDM4OTYt/YzFlNTE1MWQ0MTI4/P2ZtPWpwZyZxPTYw/Jnc9MzAwMCZpeGxp/Yj1yYi00LjEuMCZp/eGlkPU0zd3hNakEz/ZkRCOE1IeHpaV0Z5/WTJoOE9IeDhiVzkx/Ym5SaGFXNGxNakJ5/WVc1blpYeGxibnd3/Zkh3d2ZIeDhNQT09" alt=""  className="h-60 cover w-full pb-3 rounded-full shadow-black a"/>
+
         </View>
 
         <View className="gap-3">
@@ -43,6 +46,7 @@ export default function LoginScreen() {
             onPress={handleLogin}
             className="flex-row items-center justify-center gap-3 rounded-2xl bg-slate-900 py-4"
           >
+
             <Ionicons name="logo-google" size={18} color="#fff" />
             <Text className="text-base font-semibold text-white">
               Continue with Google
@@ -54,9 +58,9 @@ export default function LoginScreen() {
             onPress={handleLogin}
             className="flex-row items-center justify-center gap-3 rounded-2xl bg-teal-700 py-4"
           >
-            <Ionicons name="logo-apple" size={19} color="#fff" />
+            <Ionicons name="call-outline" size={19} color="#fff" />
             <Text className="text-base font-semibold text-white">
-              Continue with Apple
+              Continue with number
             </Text>
           </Pressable>
 
